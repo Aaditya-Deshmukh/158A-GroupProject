2a666711c7efef2f4b76dbe0e44cc0425ea77c01
Guojian Chen <kofant123@gmail.com>
2026-07-23 13:43:20 -0700

    modify part2

--- part2.py
+++ part2.py
@@ -19,26 +19,11 @@ def parse_header(reader):
     return DNSHeader(*items)
 
 
-def decode_name_simple(reader):
-    parts = []
-    while (length := reader.read(1)[0]) != 0:
-        parts.append(reader.read(length))
-    return b'.'.join(parts)
-
-
 def parse_question(reader):
-    name = decode_name_simple(reader)
+    name = decode_name(reader)
     data = reader.read(4)
     type_, class_ = struct.unpack('!HH', data)
     return DNSQuestion(name, type_, class_)
-
-
-def parse_record(reader):
-    name = decode_name_simple(reader)
-    data = reader.read(10)
-    type_, class_, ttl, data_len = struct.unpack('!HHIH', data)
-    data = reader.read(data_len)
-    return DNSRecord(name, type_, class_, ttl, data)
 
 
 def decode_name(reader):
